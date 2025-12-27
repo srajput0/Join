e5228517812b02bf42f892f027318637c7cf291e
srajput0 <pshycho566@gmail.com>
2025-12-27 17:20:49 +0530

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -15,7 +15,7 @@ from PIL import Image
 # ================= CONFIGURATION =================
 # Replace these with your actual details
 BOT_TOKEN = "8265358758:AAEh0w0gMyVadZWguiqrYQM6xegfpcy2wiA"
-MONGO_URI = "mongodb+srv://tigerbundle282: tTaRXh353IOL9mj2@testcookies.2elxf.mongodb .net/?retryWrites=true&w=majority&appName= Testcookies"
+MONGO_URI = "mongodb+srv://tigerbundle282:tTaRXh353IOL9mj2@testcookies.2elxf.mongodb.net/?retryWrites=true&w=majority&appName=Testcookies"
 OWNER_ID = 8177972152  # Your numeric Telegram ID
 GEMINI_API_KEY = "AIzaSyBfzkP06CzIMVITdEz3V6EWBcsPNvkvrHI"
 
