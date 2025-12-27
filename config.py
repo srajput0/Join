54ea1d7379926a589b0521807bf61bc43bbaed6a
srajput0 <pshycho566@gmail.com>
2025-12-27 16:26:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 # config.py
 
 # Telegram Bot Token (Get from @BotFather)
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
+BOT_TOKEN = "8265358758:AAEh0w0gMyVadZWguiqrYQM6xegfpcy2wiA"
 
 # MongoDB Connection String (Get from MongoDB Atlas)
-MONGO_URI = "YOUR_MONGO_URI_HERE"
+MONGO_URI = "mongodb+srv://tigerbundle282:tTaRXh353IOL9mj2@testcookies.2elxf.mongodb.net/?retryWrites=true&w=majority&appName=Testcookies"
 
 # Your Numeric Telegram ID (Get from @userinfobot)
-OWNER_ID = 1234567890
+OWNER_ID = 8177972152
 
 # Google Gemini API Key (Get from aistudio.google.com)
 GEMINI_API_KEY = "AIzaSyCEMtaJ-Ep3dmr-n4e4kxs7rEI28DkaWzc"
