fc2671e19d7cc12168c809aaf53d4a557330e06d
srajput0 <pshycho566@gmail.com>
2025-12-27 14:58:33 +0530

    Update join.py

--- join.py
+++ join.py
@@ -15,7 +15,7 @@ from telegram.error import BadRequest, Forbidden
 import motor.motor_asyncio
 
 # ================= CREDENTIALS =================
-BOT_TOKEN = "8265358758:AAE4xUVVEoKcfLVn-BgPhxa9kx43ATww51s"
+BOT_TOKEN = "8265358758:AAEh0w0gMyVadZWguiqrYQM6xegfpcy2wiA"
 MONGO_URI = "mongodb+srv://tigerbundle282:tTaRXh353IOL9mj2@testcookies.2elxf.mongodb.net/?retryWrites=true&w=majority&appName=Testcookies"
 DB_NAME = "TelegramBotDB"
 COLLECTION_NAME = "JoinRequests"
